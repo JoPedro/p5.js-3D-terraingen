56351dc387d4dc2825f06ee9c4e011927db1dcfe
João Pedro Silva de Queiroz <joaopedrosilvaqueiroz@gmail.com>
2020-09-26 04:25:33 -0300

    Drag and pull function (use ray casting later)

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,62 @@
+let cols, rows;
+let scl = 20;
+let w = 600;
+let h = 600;
+let zTable = [[]];
+
+function makeArray(w, h, val) {
+    var arr = [];
+    for(let i = 0; i < h; i++) {
+        arr[i] = [];
+        for(let j = 0; j < w; j++) {
+            arr[i][j] = val;
+        }
+    }
+    return arr;
+}
+
 function setup() {
     createCanvas(600, 600, WEBGL);
+    cols = w / scl;
+    rows = h / scl;
+    zTable = makeArray(cols, rows, 0)
+    for (let y = 0; y < rows; y++) {
+        for (let x = 0; x < cols; x++) {
+            zTable[x][y] = random(-10, 10);
+        }
+    }
 }
 
 function draw() {
-    background(220);
-    rect(50, 50, 50, 50);
+    background(225);
+
+    translate(width / 2, height / 2);
+    rotateX(PI/3);
+
+    translate(-width, -height);
+    for (let y = 0; y < rows; y++) {
+        beginShape(TRIANGLE_STRIP);
+        for (let x = 0; x < cols; x++) {
+            vertex(x * scl, y * scl, zTable[x][y]);
+            vertex(x * scl, (y + 1) * scl, zTable[x][y + 1]);
+        }
+        endShape();
+    }
+}
+
+function mouseDragged() {
+    for (let y = 0; y < rows; y++) {
+        for (let x = 0; x < cols; x++) {
+            if (mouseY < pmouseY) {
+                if ((mouseX - x * scl > 5) && (mouseY - y * scl > 5)) {
+                    zTable[x][y] += 5;
+                } 
+            }
+            else {
+                if ((mouseX - x * scl > 5) && (mouseY - y * scl > 5)) {
+                    zTable[x][y] -= 5;
+                }
+            }
+        }
+    }
 }
--- server.js
+++ server.js
@@ -2,4 +2,6 @@ const express = require('express')
 const app = express()
 
 const server = app.listen(3000)
+
+app.use(express.static('public'));
 console.log('Listening on 3000')
